972e118e718438d30eab6f20c5e17ec6b6cfbd99
KevinWendy <srj2513@sohu.com>
2018-01-07 11:01:02 +0800

    init a new project

---
+++ src/main/java/com/kevin/test/GitTest.java
@@ -0,0 +1,10 @@
+package com.kevin.test;
+
+/**
+ * Created by Kevin on 2018/1/7.
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("Hello Git!");
+    }
+}
